186a950f66e330f5a1e639c723bb992aed13aee6
Kaitlyn Pillay <ST10437630@vcconnect.edu.za>
2025-02-21 02:17:43 +0200

    Final details

--- MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -20,37 +20,28 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string movieGenre, string searchString)
+        public async Task<IActionResult> Index(string id)
         {
             if (_context.Movie == null)
             {
-                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+                return Problem("Entity set 'MvcMovieContext.Movie' is null.");
             }
 
-            // Use LINQ to get list of genres.
-            IQueryable<string> genreQuery = from m in _context.Movie
-                                            orderby m.Genre
-                                            select m.Genre;
-            var movies = from m in _context.Movie
-                         select m;
+            var movies = from m in _context.Movie select m;
 
-            if (!string.IsNullOrEmpty(searchString))
+            if (!String.IsNullOrEmpty(id))
             {
-                movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+                movies = movies.Where(s => s.Title!.ToUpper().Contains(id.ToUpper()));
             }
 
-            if (!string.IsNullOrEmpty(movieGenre))
+            // Create the ViewModel
+            var viewModel = new MovieGenreViewModel
             {
-                movies = movies.Where(x => x.Genre == movieGenre);
-            }
-
-            var movieGenreVM = new MovieGenreViewModel
-            {
-                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
-                Movies = await movies.ToListAsync()
+                Movies = await movies.ToListAsync(),
+                
             };
 
-            return View(movieGenreVM);
+            return View(viewModel); 
         }
 
         // GET: Movies/Details/5
--- MvcMovie/MvcMovie/Models/Movie.cs
+++ MvcMovie/MvcMovie/Models/Movie.cs
@@ -10,13 +10,12 @@ public class Movie
 
     [StringLength(60, MinimumLength = 3)]
     [Required]
-    public string? Title { get; set; }
+    public string Title { get; set; } = string.Empty;  // 🔹 Prevents NULL values
 
     [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public DateTime ReleaseDate { get; set; }
-
 
     [Range(1, 100)]
     [DataType(DataType.Currency)]
@@ -26,10 +25,10 @@ public class Movie
     [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
     [Required]
     [StringLength(30)]
-    public string? Genre { get; set; }
+    public string Genre { get; set; } = string.Empty; // 🔹 Prevents NULL values
 
     [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
     [StringLength(5)]
     [Required]
-    public string? Rating { get; set; }
+    public string Rating { get; set; } = string.Empty; // 🔹 Prevents NULL values
 }
